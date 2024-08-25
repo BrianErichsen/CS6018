c06a7a202324d1559db5944f4af65d48912ac46d
BrianErichsen <142742037+BrianErichsen@users.noreply.github.com>
2024-08-25 04:00:55 -0600

    update

--- Lab1_Hello_World/app/src/main/java/com/example/lab1/SecondActivity.kt
+++ Lab1_Hello_World/app/src/main/java/com/example/lab1/SecondActivity.kt
@@ -3,8 +3,11 @@ package com.example.lab1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Button
+import android.view.View
+import android.content.Intent
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -16,5 +19,13 @@ class SecondActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.textView).apply {
             text = buttonText
         }
+        // I added for practice a go back button that goes back to the MainActivity from
+        // the second activity
+        findViewById<Button>(R.id.go_back).setOnClickListener(this)
+    }//end of on create method
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
-}
+}//end of second activity class
